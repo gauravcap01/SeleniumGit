4ee895ba94e65a2f2f566dbde1c71136b2debf5b
gaura <gaurav.cap01@gmail.com>
2018-08-08 08:28:45 -0500

    Guru 99 Test Demo

---
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,32 @@
+package guru99.gittest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+ 
+public class AppTest {
+ 
+ public WebDriver driver;
+ 
+  @Test
+  public void openMyBlog() {
+ driver.get("https://www.softwaretestingmaterial.com/");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+   
+   System.setProperty("webdriver.gecko.driver", "C:\\Users\\gaura\\eclipse-workspace\\gittest\\drivers\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+   driver = new FirefoxDriver();
+   
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+   
+	  driver.quit();
+  }
+ 
+}
